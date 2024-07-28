1815d2c089d883cc4bbffe048af3b0d1f8b1cfd9
finger-ease <fingerease24@gmail.com>
2024-07-28 16:59:38 +0900

    fix: エラーハンドリングを修正

--- supabase/functions/src/infrastructure/persistence/repositories/playerRepository/PlayerRepository.ts
+++ supabase/functions/src/infrastructure/persistence/repositories/playerRepository/PlayerRepository.ts
@@ -8,63 +8,45 @@ import { IPlayerRepository } from "./IPlayerRepository.ts";
 
 export class PlayerRepository implements IPlayerRepository {
   public find = async (id: UniqueIdentifier): Promise<Player | null> => {
-    try {
-      const { data, error } = await supabase
-        .from("players")
-        .select("*")
-        .eq("id", id.value)
-        .single();
+    const { data } = await supabase
+      .from("players")
+      .select("*")
+      .eq("id", id.value)
+      .single();
 
-      if (error) {
-        throw error;
-      }
-
-      if (!data) {
-        return null;
-      }
-
-      return new Player(
-        new UniqueIdentifier(data.id),
-        new HitPoint(data.hit_point),
-        new AttackPoint(data.attack_point),
-        new DefensePoint(data.defense_point),
-      );
-    } catch (error) {
-      console.error(error);
+    if (!data) {
+      return null;
     }
 
-    return null;
+    return new Player(
+      new UniqueIdentifier(data.id),
+      new HitPoint(data.hit_point),
+      new AttackPoint(data.attack_point),
+      new DefensePoint(data.defense_point),
+    );
   };
 
   public store = async (player: Player): Promise<void> => {
-    try {
-      const { error } = await supabase.from("players").upsert({
-        id: player.id.value,
-        hit_point: player.hitPoint.value,
-        attack_point: player.attackPoint.value,
-        defense_point: player.defensePoint.value,
-      });
+    const { error } = await supabase.from("players").upsert({
+      id: player.id.value,
+      hit_point: player.hitPoint.value,
+      attack_point: player.attackPoint.value,
+      defense_point: player.defensePoint.value,
+    });
 
-      if (error) {
-        throw error;
-      }
-    } catch (error) {
-      console.error(error);
+    if (error) {
+      throw error;
     }
   };
 
   public delete = async (id: UniqueIdentifier): Promise<void> => {
-    try {
-      const { error } = await supabase
-        .from("players")
-        .delete()
-        .eq("id", id.value);
+    const { error } = await supabase
+      .from("players")
+      .delete()
+      .eq("id", id.value);
 
-      if (error) {
-        throw error;
-      }
-    } catch (error) {
-      console.error(error);
+    if (error) {
+      throw error;
     }
   };
 }
